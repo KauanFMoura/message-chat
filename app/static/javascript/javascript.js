2c4f7ca314bd2ffdbafe19a1230f26bdfa528ca0
KauanFMoura <135278960+KauanFMoura@users.noreply.github.com>
2024-06-25 07:20:52 -0300

    Primeira parte da integracao js/html

--- app/static/javascript/javascript.js
+++ app/static/javascript/javascript.js
@@ -55,3 +55,54 @@ window.onload = function() {
     mudouTamanho(); // Verifica o tamanho da janela e aplica as mudanças necessárias
     window.onresize = mudouTamanho; // Adiciona um listener para o evento resize
 };
+
+
+// Função para adicionar uma nova pessoa na área de conversas
+function updateConversationMenu(users, currentUser, socket) {
+    const menuConversas = document.getElementById('menu-conversas');
+    menuConversas.innerHTML = '';
+
+    users.forEach(user => {
+        if (user !== currentUser) {
+            const div = document.createElement('div');
+            div.classList.add('msg');
+            div.dataset.user = user; // Adiciona um atributo de dados para identificar o usuário
+            div.innerHTML = `
+                <img class="msg-profile" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png" alt="" />
+                <div class="msg-detail">
+                    <div class="msg-username">${user}</div>
+                </div>
+            `;
+            menuConversas.appendChild(div);
+
+            // Adiciona um evento de clique para iniciar uma conversa privada
+            div.addEventListener('click', () => {
+                startPrivateChat(currentUser, user, socket);
+            });
+        }
+    });
+}
+
+// Função para iniciar ou entrar em uma sala privada ao clicar em uma conversa
+function startPrivateChat(currentUser, otherUser, socket) {
+    const room = `${currentUser}-${otherUser}`;
+
+    // Emitir evento 'join_room' para o servidor
+    socket.emit('join_room', { username: currentUser, room: room });
+
+    // Limpar a área de chat principal
+    clearChatAreaMain();
+
+    // Emitir evento personalizado para indicar que um novo usuário foi selecionado
+    const event = new CustomEvent('privateChatStarted', { detail: { otherUser: otherUser } });
+    document.dispatchEvent(event);
+
+    // Retornar o outro usuário para o HTML
+    return otherUser;
+}
+
+// Função para limpar mensagens na área de chat principal
+function clearChatAreaMain() {
+    const chatAreaMain = document.querySelector('.chat-area-main');
+    chatAreaMain.innerHTML = ''; // Limpa o conteúdo da área de chat principal
+}
